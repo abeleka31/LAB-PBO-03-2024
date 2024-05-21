83b444661012c1e607f55ce4d51c0e8d5258ca55
abeleka31 <145075084+abeleka31@users.noreply.github.com>
2024-05-21 20:24:15 +0800

    Add files via upload

---
+++ H071231088/praktikum-8/TP8_1_H071231088/Cla.java
@@ -0,0 +1,10 @@
+
+public class Cla {
+    public static void main(String[] args) {
+        System.out.println(getTime());
+    }
+    public static int getTime(){
+        return (int) (Math.random() * 6) + 1;
+        
+    }
+}
